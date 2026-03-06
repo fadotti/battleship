2a9e1bd059456424fb7b3d95937d99ba70ec5293
Federico Dotti <fede.dotti.94@gmail.com>
2026-03-06 17:42:11 +0000

    Add landing page event handlers

--- src/event-handlers.js
+++ src/event-handlers.js
@@ -1,15 +1,31 @@
-export { addLandingPageHandlers }
+import { renderLandingPage } from "./landing-page.js";
+import { renderBoardSetup } from "./board-setup.js"; 
+import { renderArena } from "./arena.js";
+export { addLandingPageHandlers, playerName };
+
+let playerName;
 
 function addLandingPageHandlers() {
-  const playerName = document.querySelector('body > div:nth-child(2) > input');
+  const playerNameInput = document.querySelector('body > div:nth-child(2) > input');
   const startButton = document.querySelector('body > div:nth-child(3) > button');
-  playerName.addEventListener('keyup', () => {
-    switch (playerName.checkValidity()) {
+  playerNameInput.addEventListener('keyup', () => {
+    playerNameInput.setCustomValidity('');
+    switch (playerNameInput.checkValidity()) {
       case true:
         startButton.setAttribute('class', 'valid');
         break;
       default:
         startButton.removeAttribute('class');
+    }
+  })
+
+  startButton.addEventListener('click', () => {
+    if(!playerNameInput.checkValidity()) {
+      playerNameInput.setCustomValidity('Player names must: \n -Contain only upper or lower case letters. \n -Have a length that ranges between 3 and 12 characters.');
+      playerNameInput.reportValidity();
+    } else {
+      playerName = playerNameInput.value;
+      renderBoardSetup();
     }
   })
 }
